999d0214a4c1e0e493a374dbde39c567225c06fd
Defixenas <defixass@gmail.com>
2023-10-28 19:12:04 +0300

    Revert "basic form setup"
    This reverts commit 8d000ac56c349c61127c99e573188c50486695bc.

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import {Button} from "@/components/ui/button"
+import {cn} from "@/lib/utils"
+import {UserButton} from "@clerk/nextjs";
+
+const state = true;
+
+export default function Home() {
+  return (
+      <div className=''>
+          <UserButton afterSignOutUrl="/" />
+      </div>
+
+  )
+}
