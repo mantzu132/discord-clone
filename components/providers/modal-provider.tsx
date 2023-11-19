34644a6d60c688dead08a178ef0e77064267e78c
Defixenas <defixass@gmail.com>
2023-11-19 21:19:03 +0200

    delete / leave server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -6,6 +6,8 @@ import InviteModal from "@/components/modals/invite-modal";
 import EditServerModal from "@/components/modals/edit-server-modal";
 import MembersModal from "@/components/modals/members-modal";
 import CreateChannelModal from "@/components/modals/create-channel-modal";
+import LeaveServerModal from "@/components/modals/leave-server-modal";
+import DeleteServerModal from "@/components/modals/delete-server-modal";
 
 export const ModalProvider = () => {
   // preventing modals to be rendered on server side (creates inconsistencies)
@@ -25,6 +27,8 @@ export const ModalProvider = () => {
       <EditServerModal />
       <MembersModal />
       <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
     </>
   );
 };
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -80,7 +80,10 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             <DropdownMenuSeparator />
 
             {isAdmin && (
-              <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+              <DropdownMenuItem
+                onClick={() => onOpen("deleteServer", { server })}
+                className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+              >
                 Delete Server
                 <Trash className="h-5 w-5 ml-auto" />
               </DropdownMenuItem>
@@ -89,7 +92,10 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
 
         {!isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("leaveServer", { server })}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
             Leave Server
             <LogOut className="h-5 w-5 ml-auto" />
           </DropdownMenuItem>
